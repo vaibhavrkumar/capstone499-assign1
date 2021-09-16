4019b44d9608e6774810e4d8781a5c8de8c4b76c
Vaibhav R. Kumar <vaibhavk667@gmail.com>
2021-09-16 14:34:09 -0700

    descending sort

--- cosc499-a1/src/mainClass.java
+++ cosc499-a1/src/mainClass.java
@@ -7,12 +7,28 @@ public class mainClass {
 		int [] arr = {12, 87, 78, 98,23, 45};
 		
 		sortArrayAsc(arr);
-	}
+		sortArrayDesc(arr);
+	}	
 
 	private static void sortArrayAsc(int[] arr) {
 		System.out.printf("Original Array: %s", Arrays.toString(arr));
 		Arrays.sort(arr);
 		System.out.println();
 		System.out.printf("Sorted in Ascending: %s", Arrays.toString(arr));
+	}
+	
+	private static void sortArrayDesc(int[] arr) {
+		// Sorts IntArray in descending order 
+		for (int i = 0; i < arr.length; i++) {
+	        for (int j = i + 1; j < arr.length; j++) {
+	            int tmp = 0;
+	            if (arr[i] > arr[j]) {
+	                tmp = arr[i];
+	                arr[i] = arr[j];
+	                arr[j] = tmp;
+	            }
+	        }
+	    }
+	System.out.printf("Sorted in Descending: %s", Arrays.toString(arr));	
 	}
 }
